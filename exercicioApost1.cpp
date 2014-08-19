ae8488604d852ae7e675701ecdcf54c6b9eb8787
DinSidney <dinsidney@gmail.com>
2014-08-18 23:30:33 -0300

    primeiro commit

---
+++ exercicioApost1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Alo, Mundo!\n";
+    cout << "Eis um numero: " << 42 << "\n";
+    cout << "Um numero grande: "   << 280830583058 << "\n";
+    cout << "Eis uma soma: "
+         << "Soma de 245 + 432 = "
+         << 245 + 432
+         << "\n";
+    return 0;
+} //fim da função main
