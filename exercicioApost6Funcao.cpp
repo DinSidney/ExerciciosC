5b02705b5cbd121fced3fb6655a84b3b073f438a
DinSidney <dinsidney@gmail.com>
2014-08-24 20:13:36 -0300

    primeiro exemplo de funcao

---
+++ exercicioApost6Funcao.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void digaAlo() // cria função digaAlo.
+{
+    cout << "\nAlo, mundo!";
+}               //fim função digaAlo.
+
+int main()
+{
+    digaAlo();  //chama a função.
+    return 0;
+}
