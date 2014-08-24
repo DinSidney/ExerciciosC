5e4162b8dae34584877f03fe53182f48a701de49
DinSidney <dinsidney@gmail.com>
2014-08-24 19:14:29 -0300

    concatenação de strings

---
+++ exercicioApost5String.cpp
@@ -0,0 +1,15 @@
+// usa string e concatena com o simbolo +
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1 = "Agua mole ";
+    string s2 = "em pedra dura ";
+    string s3 = "tanto bate ";
+    string s4 = "ate que fura ";
+    string s5 = "um dia eu aprendo C++ ";
+
+    cout << s1 + s2 + s3 + s4 + s5 + "!!!\n\n"; // mostra o conteudo da string.
+ }
