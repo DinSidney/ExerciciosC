d192b55399743aed935116b5d963dccf1915e3c9
DinSidney <dinsidney@gmail.com>
2014-08-26 22:52:07 -0300

    Funcao com parametros

---
+++ exercicioApost7FuncPar.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+float Delta(float a, float b, float c)
+{
+
+}
+
+float Zeros(float x1, float x2)
+{
+
+}
+
+int main()
+{
+   cout << "Estamos em main()\n";
+   float a, b, c;
+   cout << "\n digite o valor a = ";
+   cin >> a;
+   cout << "\n digito o valor b = ";
+   cin >> b;
+
+    return 0;
+}
